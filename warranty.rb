6bf13de032528099d8daf7b4a937872efe281956
Rusty Myers <rustymyers@gmail.com>
2011-02-12 13:39:09 -0500

    adding code to write details to plist

--- warranty.rb
+++ warranty.rb
@@ -5,8 +5,17 @@
 #              	about your product. Accepts arguments of machine serial numbers.
 # Edit:		This is a fork @glarizza's script:
 # 		https://github.com/huronschools/scripts/blob/master/ruby/warranty.rb
+# Edit:   This is a fork of @chilcote's script:
+#     https://github.com/chilcote/warranty
+#     Adding functions to write details to plists
 
 require 'open-uri'
+require "osx/cocoa"
+require 'pp'
+
+myfile = 'appwarranty.plist'
+my_dict = OSX::NSMutableDictionary.dictionary
+
 
 def get_warranty(serial)
   hash = {}
@@ -18,9 +27,10 @@ def get_warranty(serial)
     }
     
     puts "\nSerial Number:\t\t#{hash['SERIAL_ID']}\n"
-    puts "Product Decription:\t#{hash['PROD_DESCR']}\n"
+    puts "Product Description:\t#{hash['PROD_DESCR']}\n"
     puts "Purchase date:\t\t#{hash['PURCHASE_DATE'].gsub("-",".")}"
     puts (!hash['COV_END_DATE'].empty?) ? "Coverage end:\t\t#{hash['COV_END_DATE'].gsub("-",".")}\n" : "Coverage end:\t\tEXPIRED\n"
+  
   }
   
 # Import the latest list of ASD versions and match the PROD_DESCR with the correct ASD
@@ -30,6 +40,16 @@ def get_warranty(serial)
     asd_hash[asd_arrary[0]] = asd_arrary[1]
   end
   puts "ASD Version:\t\t#{asd_hash[hash['PROD_DESCR']]}\n"
+  
+  myfile = 'appwarranty.plist'
+  my_dict = OSX::NSMutableDictionary.dictionary
+  my_dict['Product Decription'] = "#{hash['PROD_DESCR']}"
+  my_dict['Purchase date'] = "#{hash['PURCHASE_DATE'].gsub("-",".")}"
+  my_dict['Coverage end'] = (!hash['COV_END_DATE'].empty?) ? "#{hash['COV_END_DATE'].gsub("-",".")}" : "EXPIRED"
+  my_dict['ASD version'] = "#{asd_hash[hash['PROD_DESCR']].gsub("\n","")}"
+  my_dict.writeToFile_atomically(myfile, true)
+  pp my_dict
+
 end
 
 if ARGV.size > 0 then
@@ -38,7 +58,7 @@ if ARGV.size > 0 then
   end
 else
   puts "Without your input, we'll use this machine's serial number."
-  serial = %x(system_profiler SPHardwareDataType |grep -v tray |awk '/Serial/ {print $4}').upcase.chomp
+  serial = %x(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}').upcase.chomp
   get_warranty(serial)
 end
 
