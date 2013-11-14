b2fd99ec4a9ba77169b6cce64cff392af4274ac6
Rusty Myers <rustymyers@gmail.com>
2013-11-14 15:04:17 -0500

    Updated code to allow for CSV with computer name. outputting to csv named output.csv now.

--- seagatewarrantycheck.sh
+++ seagatewarrantycheck.sh
@@ -7,19 +7,23 @@
 # Credit to brunerd for curl: http://www.brunerd.com/blog/2012/10/16/check-multiple-imacs-for-1tb-seagate-repair-program/
 #curl  "https://supportform.apple.com/201107/SerialNumberEligibilityAction.do?cb=iMacHDCheck.response&sn=$(ioreg -c "IOPlatformExpertDevice" | awk -F '"' '/IOPlatformSerialNumber/ {print $4}')" 2>/dev/null
 
+# Expects csv with headers "ComputerName,SerialNumber". Also requires Unix (LF) formatting.
+
 VERBOSE=   #Set to 1 to see verbose
 
 # Pass list of serials as first argument
 for i in $(cat "${1}"); do
 
+COMPNAME=$(echo ${i} | awk -F, '{print $1}')
+COMPSERIAL=$(echo ${i} | awk -F, '{print $2}')
+
 if [ "${VERBOSE}" ]; then 
-    echo "VERBOSE"
-	echo "Checking ${i}"
+	echo "Checking: $COMPNAME  -  Serial: $COMPSERIAL"
 fi
 
-SerialNumber="${i}"
-echo -n "SerialNumber: $SerialNumber   -- "
-curl "https://supportform.apple.com/201107/SerialNumberEligibilityAction.do?cb=iMacHDCheck.response&sn=${SerialNumber}" 2>/dev/null | awk -F: '{print $3}' | tr -d \"}\) 
+COMPSTATUS=`curl "https://supportform.apple.com/201107/SerialNumberEligibilityAction.do?cb=iMacHDCheck.response&sn=${COMPSERIAL}" 2>/dev/null | awk -F: '{print $3}' | tr -d \"}\)`
+
+echo "$COMPNAME,$COMPSTATUS" >> output.csv
 
 done
 
